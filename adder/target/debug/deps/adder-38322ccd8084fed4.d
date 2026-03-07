74305f3ab8733dbd65d190def0cc049867c57ca7
qiaoluhao <3953639144@qq.com>
2026-03-07 18:30:22 +0800

    第十一章：编写自动化测试

---
+++ adder/target/debug/deps/adder-38322ccd8084fed4.d
@@ -0,0 +1,5 @@
+F:\rust\adder\target\debug\deps\adder-38322ccd8084fed4.d: src\lib.rs
+
+F:\rust\adder\target\debug\deps\adder-38322ccd8084fed4.exe: src\lib.rs
+
+src\lib.rs:
---
+++ adder/target/debug/deps/adder-3c7b508fddfa161d.d
@@ -0,0 +1,7 @@
+F:\rust\adder\target\debug\deps\adder-3c7b508fddfa161d.d: src\lib.rs
+
+F:\rust\adder\target\debug\deps\libadder-3c7b508fddfa161d.rlib: src\lib.rs
+
+F:\rust\adder\target\debug\deps\libadder-3c7b508fddfa161d.rmeta: src\lib.rs
+
+src\lib.rs:
