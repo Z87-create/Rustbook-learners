8159377805b3191c7ac420512c567aaac3b4c57f
qiaoluhao <3953639144@qq.com>
2026-03-08 22:44:26 +0800

    第十二章:一个I/O项目：构建命令行程序

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,27 +7,8 @@ fn main(){
         eprintln!("Problem parsing arguments:{}",err);
         process::exit(1);
     });
-    // println!("Problem parsing arguments:{}",err);
-    // process::exit(1);
-    // run(config);
-    // let query=&args[1];
-    // let filename=&args[2];
-    // println!("{:?}",args);
-    println!("Searching for {}",config.query);
-    println!("In file {}",config.filename);
     if let Err(e)=minigrep::run(config){
         eprintln!("Application error:{}",e);
         process::exit(1);
     }
-    // if let Err(e)=run(Config){
-    //     println!("Application error:{}",e);
-    //     process::exit(1);
-    // }
-    // let contents=fs::read_to_string(filename).expect("Something went wrong reading the file");
-    // println!("With text:\n{}",contents);
-}
-fn parse_config(args:&[String])->(&str,&str){
-    let query=&args[1];
-    let filename=&args[2];
-    (query,filename)
 }
